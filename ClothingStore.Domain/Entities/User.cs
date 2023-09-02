20469a5fe5826df121a97c7d43921c9ee4ea003c
Oleg Voroshylov <olegvoroshilov123@gmail.com>
2023-09-02 20:15:19 +0300

    Add role to user

--- ClothingStore.Domain/Entities/User.cs
+++ ClothingStore.Domain/Entities/User.cs
@@ -1,10 +1,12 @@
-﻿namespace ClothingStore.Domain.Entities
+﻿using ClothingStore.Domain.Enums;
+
+namespace ClothingStore.Domain.Entities
 {
     public class User
     {
         public int Id { get; set; }    
         public string Login { get; set; }
         public string Password { get; set; }
-
+        public UserRole Role { get; set; }
     }
 }
