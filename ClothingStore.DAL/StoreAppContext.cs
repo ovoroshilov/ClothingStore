f8a0526b2bf09ed117b4f74c137d5a0de2619845
Oleg Voroshylov <olegvoroshilov123@gmail.com>
2023-09-03 11:39:00 +0300

    Add default user

--- ClothingStore.DAL/StoreAppContext.cs
+++ ClothingStore.DAL/StoreAppContext.cs
@@ -1,5 +1,8 @@
 ﻿using ClothingStore.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using ClothingStore.Domain.Enums;
+using ClothingStore.Domain.Helpers;
+
 namespace ClothingStore.DAL
 {
     public class StoreAppContext : DbContext
@@ -12,10 +15,27 @@ namespace ClothingStore.DAL
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<User>(builder =>
+            {
+                builder.HasData(new User
+                {
+                    Id = 1,
+                    Login = "OVoroshilov",
+                    Password = HashPasswordHelper.HashPassword("password"),
+                    Role = UserRole.Admin
+                });
+                builder.ToTable("Users").HasKey(x => x.Id);
+
+                builder.Property(x => x.Login).IsRequired().HasMaxLength(50);
+
+                builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            });
+
+
             modelBuilder.Entity<Product>(entity =>
             {
                 entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
-            });   
+            });
         }
     }
 }
