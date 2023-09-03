262927c4f3426b3f29fd98a21454349a98bc52be
Oleg Voroshylov <olegvoroshilov123@gmail.com>
2023-09-03 11:39:53 +0300

    Add password hashing

---
+++ ClothingStore.Domain/Helpers/HashPasswordHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ClothingStore.Domain.Helpers
+{
+    public static class HashPasswordHelper
+    {
+       public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var hashedPassword = BitConverter.ToString(bytes).Replace("-", "").ToLower();
+                return hashedPassword;
+            };
+        }
+    }
+}
