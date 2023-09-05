6254b1a216c60e54b92a8736cb47b5b55ac38888
Oleg Voroshylov <olegvoroshilov123@gmail.com>
2023-09-06 02:20:02 +0300

    Add logic for registration

---
+++ ClotherStore.Services/Implementions/AccountService.cs
@@ -0,0 +1,77 @@
+﻿using ClotherStore.Services.Interfaces;
+using ClothingStore.DAL.Repositories;
+using ClothingStore.Domain.Entities;
+using ClothingStore.Domain.Response;
+using ClothingStore.Domain.ViewModels.Account;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+using ClothingStore.Domain.Enums;
+using ClothingStore.Domain.Helpers;
+
+namespace ClotherStore.Services.Implementions
+{
+    public class AccountService : IAccountService
+    {
+        private readonly UserRepository _userRepository;
+        private readonly ILogger<AccountService> _logger;
+
+        public AccountService(UserRepository userRepository, ILogger<AccountService> logger)
+        {
+            _userRepository = userRepository;
+            _logger = logger;
+        }
+
+        public Task<IBaseResponse<ClaimsIdentity>> LogIn(RegisterViewModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IBaseResponse<ClaimsIdentity>> Register(RegisterViewModel user)
+        {
+            try
+            {
+                var newUser = await _userRepository.GetAll().FirstOrDefaultAsync(x => x.Login == user.Login);
+                if (newUser != null)
+                {
+                    return new BaseResponse<ClaimsIdentity>
+                    {
+                        Description = "User with this name already exists"
+                    };
+                }
+                newUser = new User()
+                {
+                    Login = user.Login,
+                    Password = HashPasswordHelper.HashPassword(user.Password),
+                    Role = UserRole.Custumer
+                };
+                await _userRepository.Create(newUser);
+                var result = Authenticate(newUser);
+
+                return new BaseResponse<ClaimsIdentity>
+                {
+                    Description = "Account has succeflully created"
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"[Register]: {ex.Message}");
+                return new BaseResponse<ClaimsIdentity>
+                {
+                    Description = "Account has succeflully created",
+                    StatusCode = StatusCode.InternalServerError
+                };
+            }
+        }
+        private ClaimsIdentity Authenticate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.ToString())
+            };
+            return new ClaimsIdentity(claims, "ApplicationCookie",
+                ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+        }
+    }
+}
---
+++ ClothingStore.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using ClothingStore.DAL.Interfaces;
+using ClothingStore.Domain.Entities;
+
+namespace ClothingStore.DAL.Repositories
+{
+    public class UserRepository : IBaseRepository<User>
+    {
+        private readonly StoreAppContext _storeAppContext;
+
+        public UserRepository(StoreAppContext storeAppContext)
+        {
+            _storeAppContext = storeAppContext;
+        }
+
+        public async Task Create(User entity)
+        {
+            await _storeAppContext.Users.AddAsync(entity);
+            await _storeAppContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(User entity)
+        {
+            _storeAppContext.Users.Remove(entity);
+            await _storeAppContext.SaveChangesAsync();
+        }
+
+        public async Task<User> Update(User entity)
+        {
+            _storeAppContext.Users.Update(entity);
+            await _storeAppContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return _storeAppContext.Users;
+        }
+    }
+}
