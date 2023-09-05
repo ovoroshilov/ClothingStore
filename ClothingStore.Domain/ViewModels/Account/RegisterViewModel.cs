f6643bf0618b1b5ed7eb16a9f8bcb06de22f8e05
Oleg Voroshylov <olegvoroshilov123@gmail.com>
2023-09-06 02:20:34 +0300

    Add register viemodel

---
+++ ClothingStore.Domain/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClothingStore.Domain.ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Enter username")]
+        public string Login { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Enter password")]
+        [MinLength(5, ErrorMessage ="Password has to have at least 5 symbols")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Confirm password")]
+        [Compare("Password" , ErrorMessage = "Passwords are not same")]
+        public string PasswordConfirm { get; set; }
+    }
+}
